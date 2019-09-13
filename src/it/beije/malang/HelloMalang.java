9ad9a3d8a52c4dba731804c24ee6452b4e238ec6
Padawan07 <Padawan07@DESKTOP-6C7CK3K>
2019-09-13 15:46:05 +0200

    "sono il ronaldinho dei commit"

--- src/it/beije/malang/HelloMalang.java
+++ src/it/beije/malang/HelloMalang.java
@@ -5,6 +5,7 @@ public class HelloMalang {
 	public static void main(String[] args) {
 		System.out.println("Hello Malang!!");
 		System.out.println("Hello Malang!!");
+		System.out.println("Sono Ronaldinho");
 	}
 
 }
