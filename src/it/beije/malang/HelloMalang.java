d4708ab9fe714d9d61b8af37b52905da78b30612
Padawan05 <Padawan05@DESKTOP-RSATDJO>
2019-09-13 15:45:30 +0200

    My very first time

--- src/it/beije/malang/HelloMalang.java
+++ src/it/beije/malang/HelloMalang.java
@@ -3,8 +3,7 @@ package it.beije.malang;
 public class HelloMalang {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Malang!!");
-		System.out.println("Hello Malang!!");
+		System.out.println("Hello Padawan!");
 	}
 
 }
