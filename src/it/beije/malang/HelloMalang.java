973aeab0c15d392e520443301fd3a742b8e42a1c
Padawan13 <Padawan13@DESKTOP-LJLDQVK>
2019-09-13 16:03:34 +0200

    Modifica

--- src/it/beije/malang/HelloMalang.java
+++ src/it/beije/malang/HelloMalang.java
@@ -3,7 +3,7 @@ package it.beije.malang;
 public class HelloMalang {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Malang!!");
+		System.out.println("Hello 13!!");
 	
 	}
 
