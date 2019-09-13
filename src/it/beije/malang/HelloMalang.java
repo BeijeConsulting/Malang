228ae1a5987ee59099fffae713c03c513e1e672e
simoneagostini <1996enomis@gmail.com>
2019-09-13 15:45:37 +0200

    Primo commit

--- src/it/beije/malang/HelloMalang.java
+++ src/it/beije/malang/HelloMalang.java
@@ -5,6 +5,9 @@ public class HelloMalang {
 	public static void main(String[] args) {
 		System.out.println("Hello Malang!!");
 		System.out.println("Hello Malang!!");
+		System.out.println("Hello Malang!!");
+		
+		System.out.println("Hello Malang!!");
 	}
 
 }
