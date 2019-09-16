a4dbf65d12c99f209632a203c3aa2443d36a3487
vittorioluoni <vittorio.luoni@libero.it>
2019-09-16 14:54:12 +0200

    qualche aggiornamento

--- src/it/beije/malang/EqualsAndUgualeUguale.java
+++ src/it/beije/malang/EqualsAndUgualeUguale.java
@@ -1,4 +1,5 @@
 package it.beije.malang;
+import java.util.*;
 
 public class EqualsAndUgualeUguale {
 
@@ -16,6 +17,26 @@ public class EqualsAndUgualeUguale {
 			//System.out.println("s1==sb"+ s1==sb);
 			System.out.println("s1.equals(sb)"+ s1.equals(sb1));
 			System.out.println("sb2.equals(sb1)"+ sb2.equals(sb1));
+			List<String> list1 = new ArrayList<String>();
+			list1.add("qui");
+			list1.add("quo");
+			list1.add("qua");
+			List<String> list2 = new ArrayList<String>(list1);
+			System.out.println("list1==list2"+ (list1==list2));//false
+			System.out.println("list1.equals(list2)"+ list1.equals(list2));//true
+			
+			List<String> list4 = list1;
+			System.out.println("list1==list4"+ (list1==list4));//true
+			System.out.println("list1.equals(list4)"+ list1.equals(list4));//true
+			
+			List<String> list3 = new ArrayList<String>(list1);//se facessi quest utima cosa con gli stringbuilder
+			//mi darebbe false anche l' ultimo. percheè farebbe equals tra due stringbuilder e quindi starei guardando i riferimenti
+			list3.add(new String("qui"));
+			list3.add(new String("quo"));
+			list3.add(new String("qua"));
+			System.out.println("list1==list3"+ (list1==list3));//false
+			System.out.println("list1.equals(list3)"+ list1.equals(list3));//true
+		
 	}
 
 }
--- src/it/beije/malang/EserciziStringBuilder.java
+++ src/it/beije/malang/EserciziStringBuilder.java
@@ -6,6 +6,7 @@ public class EserciziStringBuilder {
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder("Ronaldinho");
 		StringBuilder sb1 = new StringBuilder("Ronaldinho");
+		System.out.println(sb);// per chiedere la lunghezza della stringa devo usare length non size
 		System.out.println(sb.append("Frenklyn"));
 		System.out.println(sb.length());
 		System.out.println(sb.charAt(6));
@@ -21,5 +22,14 @@ public class EserciziStringBuilder {
 		System.out.println(s.equals(r));
 		System.out.println(s == r);
 		System.out.println(s == n);
+		
+		
+		int total = 0;
+		StringBuilder letters = new StringBuilder("abcdefg");
+		total += letters.substring(1, 2).length();
+		total += letters.substring(6, 6).length();
+		total += letters.substring(6, 5).length();
+		System.out.println(total);
+		char[]c = new char[2];
 	}
 }
