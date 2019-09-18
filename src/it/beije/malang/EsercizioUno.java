2addbcc5e09bc4f96c749609a873e71026290981
et994 <etoax94@gmail.com>
2019-09-18 16:47:56 +0200

    Upload EsercizioUno da consegne via mail

---
+++ src/it/beije/malang/EsercizioUno.java
@@ -0,0 +1,37 @@
+import java.lang.Math;
+import java.util.*;
+
+public class EsercizioUno {
+
+	public static void main(String... args) {
+		System.out.println("\nScegliere tipo di operazione: \n");
+		System.out.println("\n1. Addizione\n2. Sottrazione\n3.Moltiplicazione\n4. Divisione\n");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double operazione = sc.nextDouble();
+		
+		if(operazione == 1) {
+			
+		}
+			
+	}
+
+
+	public static double addizione(double... n){
+
+	}
+
+	public static double sottrazione(double... n){
+
+	}
+
+	public static double moltiplicazione(double... n){
+
+	}
+
+	public static double divisione(double... n){
+
+	}
+
+}
