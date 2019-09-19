df874f316ae57aed9efed573d45d5b2c8258ccce
ivfly <iv0fly@gmail.com>
2019-09-19 16:37:15 +0200

    file manager + contatto

---
+++ src/it/beije/malang/Contatto.java
@@ -0,0 +1,47 @@
+package it.beije.malang;
+
+public class Contatto {
+	
+	private String cognome;
+	private String nome;
+	private String telefono;
+	private String email;
+
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("cognome : ").append(cognome).append('\n');
+		builder.append("nome : ").append(nome).append('\n');
+		builder.append("telefono : ").append(telefono).append('\n');
+		builder.append("email : ").append(email).append('\n');
+		return builder.toString();
+	}
+}
