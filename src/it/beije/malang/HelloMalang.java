e6794abaa8668ec6e8bd695467e7a9b45e113612
fradona00 <francesco.donatellis00@gmail.com>
2019-09-19 14:33:44 +0200

    modifica

--- src/it/beije/malang/HelloMalang.java
+++ src/it/beije/malang/HelloMalang.java
@@ -4,10 +4,7 @@ public class HelloMalang {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Malang!!");
-<<<<<<< HEAD
-		System.out.println("Hello Malang!1!");
-=======
->>>>>>> refs/remotes/origin/master
+		System.out.println("Hello Malang!!");
 	}
 
 }
