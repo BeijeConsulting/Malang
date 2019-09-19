865a05bfab02a3a0e30083a7c70d5d84c6d6952f
Ferulligabriele <55279860+Ferulligabriele@users.noreply.github.com>
2019-09-19 16:46:50 +0200

    prove

---
+++ src/it/beije/malang/Parallelepipedo.java
@@ -0,0 +1,13 @@
+package it.beije.malang;
+import java.util.*;
+public class Parallelepipedo {
+	private int lunghezza;
+	private int larghezza;
+	private int altezza;
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
