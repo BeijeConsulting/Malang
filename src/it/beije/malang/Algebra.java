1b97dd1fe329d26a0bd4c7b4cf22630acf541e17
Franklin9917 <franklinscigliano97@gmail.com>
2019-09-20 17:28:59 +0200

    aggiunta classe algebra.java

--- src/it/beije/malang/Algebra.java
+++ src/it/beije/malang/Algebra.java
@@ -3,49 +3,71 @@ package it.beije.malang;
 import java.util.ArrayList;
 public class Algebra {
 	
-	public static void main(String[] args) {
-		String c =.equals("+") ;
+	     public static void main(String[] args) {
+	   
+	    int somma= 0; //
+	    
+	    int [] numeri ={1, 2, 3, 10 };//ARRAY ADDIZIONE
+	    int [] number = {10,2,4,2};//ARRAY MOLTIPLICAZIONE
+	    int [] number1 ={10, 15,};//ARRAY SOTTRAZIONE
+	    int [] number2 = {30,10, 3};//DIVISIONE
+	    
+	    /*int x = number[0];//numero molt
+	    int y = number[1];//numero molt
+	    int z = x*y;// moltiplicazione*/
+	    
+	    int z=number[0]; // moltiplicazione
+	    for (int i=1;i<number.length;i++)
+	        z=z*number[i];
+	        
+	    int x =number1[0];// sottrazione
+	    for (int a=1; a<number1.length; a++)
+	        x=x-number1[a];
+	        
+	    int y =number2[0];//divisione
+	    for (int c=1; c<number2.length; c++)
+	        y =y/number2[c];
+	    
+	    
+	    
+	   // int num1 = number1[0];//numero SOTT
+	    //int num2 = number1[1];//numero SOTT
+	    //int num3 = num1-num2;//SOTTRAZIONE
+	    
+	    //int num4 =number2[0];//numero div
+	    //int num5 =number2[1];//numero div
+	    //int num6 =num4 / num5;//DIVISIONE
+	    
+	    
+	    
+	    //for (int i = 0; i< numeri.length; i++) {
+	    for (int d : numeri) { //
+	    somma= somma +d;//
+	   
+	   }
+	    System.out.println("SOMMA: "+ somma); //
+	    somma=0;//
+	    //for (int e : number) {
+	       
+	        //no:   int c = a*b;
+	        //no:   moltiplicazione = 
+	       
+	       
+	         System.out.println("MOLTIPLICAZIONE:" + z);
+	         System.out.println("SOTTRAZIONE:" + x);
+	         System.out.println("DIVISIONE:" +y);
+	    }
+	    }
+	      
+	    
+	   // }
+	   
+	     
+	  //public static void main(String[] args) {
+	    //int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+	    //int x = myNumbers[1][0];
+	    //System.out.println(x);  
+	 	
 	
-			
-	
-		String numero = args[1];
-		int intero = Integer.parseInt(numero);
-		
-		String num2 = args[2];
-		Integer valore = Integer.parseInt(num2);
-		
-		String num3= args[3];
-		Integer val2= Integer.parseInt(num3);
-		
-		String num4= args[4];
-		Integer val3= Integer.parseInt(num4);
-		
-		int s = intero+valore+val2 ;
-		
-		System.out.println(s);
-		
-		//System.out.println(args[0]);
-		//System.out.println(args[1]);
-		//System.out.println(args[2]);
+	  
 
-		
-		
-ArrayList<String> somma = new ArrayList<String>(); 
-	//somma.add("+");
-	somma.add("5");
-	somma.add("10");
-	somma.add("2");
-	
-	
-	
-	
-	
-	//System.out.println(somma.indexOf("+"));
-	
-	
-    
-		
-		
-	}
-
-}
