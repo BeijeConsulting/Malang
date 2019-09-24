8b2c308349a44635b331042185c3876b903add3f
simoneagostini <1996enomis@gmail.com>
2019-09-24 17:26:40 +0200

    eccezioni

---
+++ src/it/beije/malang/Esempioeccezioni.java
@@ -0,0 +1,26 @@
+package it.beije.malang;
+
+public class Esempioeccezioni {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int a;
+		int b;
+		
+		
+		try {
+			
+			a=Integer.parseInt(args[0]);
+			b=Integer.parseInt(args[1]);
+			
+			
+		}
+		
+		catch(Exception e) 
+		{System.out.println("errore");}
+	}
+	
+	
+}
