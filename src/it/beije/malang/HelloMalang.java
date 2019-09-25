bbe682facbcc446e466bb1ccd89d594ace40d8d5
FrancescoCorbosiero <francesco.corbosiero00@gmail.com>
2019-09-25 13:11:53 +0200

    .

--- src/it/beije/malang/HelloMalang.java
+++ src/it/beije/malang/HelloMalang.java
@@ -4,10 +4,7 @@ public class HelloMalang {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Malang!!");
-<<<<<<< HEAD
 		System.out.println("Hello Sisssndoro!!");
-=======
->>>>>>> refs/remotes/origin/master
 	}
 
 }
