96a9a2e2e16a6f109d21f522656daf07cd8c891a
Padawan14 <Padawan14@DESKTOP-UQ067PO>
2019-09-25 15:38:21 +0200

    Loss of data double parser

--- src/it/beije/malang/MyParser.java
+++ src/it/beije/malang/MyParser.java
@@ -1,7 +1,7 @@
 package it.beije.malang;
 
 public class MyParser {
-	
+
 	public static int parseInt(String num) {
 		int val = 0;
 		for (int i=0; i < num.length(); i++) {
@@ -12,30 +12,57 @@ public class MyParser {
 				throw new IllegalArgumentException("il valore inserito non è numerico");
 			}
 		}
-		
-		return val;
-	}
-	
-	public static double parseDouble(String num) {
-		double val = 0;
-		
-		//....
-		
+
 		return val;
 	}
 
+	public static double parseDouble(String num) {
+		double val = 0;
+		double init = 0.0;
+		double floatpoint = 0.0;
+		String[] arrstr = num.split("\\.");
+
+		if (arrstr.length > 2) throw new IllegalArgumentException();
+		for (int i=0; i < arrstr[0].length(); i++) {
+			char c = arrstr[0].charAt(i);
+			if (c >= '0' && c <= '9') {
+				init = (init*10) + (c - 48);
+			} else {
+				throw new IllegalArgumentException("il valore inserito non è numerico");
+			}
+		}
+		//System.out.println(init);
+		if(arrstr.length == 2) {
+			for (int i=0; i < arrstr[1].length(); i++) {
+				char c = arrstr[1].charAt(i);
+				if (c >= '0' && c <= '9') {
+					floatpoint = (double) (c - 48) / (double) (10 * (i + 1))+floatpoint;
+					//System.out.println(floatpoint);
+				} else {
+					throw new IllegalArgumentException("il valore inserito non è numerico");
+				}
+
+
+			}
+		}
+		return init + floatpoint;
+	}
+
+
+
 	public static void main(String[] args) {
 		//System.out.println(parseInt("5142"));
-		
+
 		System.out.println(parseDouble("56"));
-//		System.out.println(parseDouble("5.6"));
-//		System.out.println(parseDouble("53.76"));
-//		System.out.println(parseDouble("5.60"));
-//		System.out.println(parseDouble("54.6"));
-//		System.out.println(parseDouble("0.6"));
-//		System.out.println(parseDouble("5.6.8"));
-//		System.out.println(parseDouble(".56"));
-//		System.out.println(parseDouble("56."));
+		System.out.println(parseDouble("5.6"));
+		System.out.println(parseDouble("53.76"));
+		System.out.println(parseDouble("5.60"));
+		System.out.println(parseDouble("54.6"));
+		System.out.println(parseDouble("0.6"));
+		System.out.println(parseDouble("5.6.8"));
+		System.out.println(parseDouble(".56"));
+		System.out.println(parseDouble("56."));
+		
 	}
 
 }
