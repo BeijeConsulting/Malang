8b48562bb155efbf03dc279d8ee857df788b4fed
FrancescoCorbosiero <francesco.corbosiero00@gmail.com>
2019-09-25 15:13:05 +0200

    Esericizio conversione int e double

--- src/it/beije/malang/Parser.java
+++ src/it/beije/malang/Parser.java
@@ -3,8 +3,10 @@ package it.beije.malang;
 public class Parser {
 
     public static int parseInt(String s) {
-        //val*10 + c-48
         int res = 0;
+        
+        if(s.isEmpty())
+        	throw new IllegalArgumentException("Wrong string format");
 
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
@@ -23,17 +25,19 @@ public class Parser {
             return parseInt(n);
         else
         {
-            if(n.indexOf('.') != n.lastIndexOf('.'))
+        	String[] s = n.split("\\.");
+        	String s1 = s[0];
+        	String s2 = s[1];
+        	
+            if(s.length > 2)
                 throw new IllegalArgumentException("String contains multiple floating points");
-
-            String s1 = n.substring(0, n.indexOf('.'));
-            String s2 = n.substring(n.indexOf('.'));
+           
             res += parseInt(s1);
 
-            for(int i = 1; i < s2.length(); i++){
+            for(int i = 0; i < s2.length(); i++){
                 char c = s2.charAt(i);
-                if(Character.isDigit(c))
-                    res += (c - 48) * Math.pow(10, -i);
+                if(Character.isDigit(c)) 
+                    res += (c - 48) * Math.pow(10, -(i + 1));
                 else
                     throw new IllegalArgumentException("String contains NaN values");
             }
@@ -43,9 +47,8 @@ public class Parser {
     }
 
     public static void main(String... args){
-        System.out.println(parseDouble("150.055"));
-        //System.out.println(parseDouble(".150.150"));
-        //System.out.println(parseDouble("15A0.150"));
+       
+    	System.out.println(parseDouble("111.399A"));
     }
 
 }
