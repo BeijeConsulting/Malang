b1d5b091cab4b30db2331db6d04589d0b87c6cae
Fulginiti <Fulginiti@LAPTOP-EE4JK8P8>
2019-10-02 14:52:08 +0200

    cavallo

--- src/it/beije/malang/natura/CavalloMarco.java
+++ src/it/beije/malang/natura/CavalloMarco.java
@@ -7,13 +7,13 @@ public class CavalloMarco extends MammiferiMarco{
 	}
 	
 	public void riproduzione() {
-		System.out.println("il parto avviene in modo classico, espellendo il puledro partorendo sulle 4 zampe");
+		System.out.println("il parto avviene in modo classico, espellendo il puledro partorendo sulle 4 zampe \n");
 	}
 	public void movimento() {
-		System.out.println("galoppo,trotto o passo sono le tipologie di spostamento");
+		System.out.println("galoppo,trotto o passo sono le tipologie di spostamento \n");
 	}
 	public void respirazione() {
-		System.out.println("normali narici collegate con trache e polmoni,respirzione classica");
+		System.out.println("normali narici collegate con trache e polmoni,respirzione classica \n");
 	}
 	
 	public static void main(String... args) {
