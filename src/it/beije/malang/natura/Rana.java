b6d635b1d1515c4421cbeb8a472d7edbd4a515fe
simoneagostini <1996enomis@gmail.com>
2019-10-02 15:36:44 +0200

    classe rana

--- src/it/beije/malang/natura/Rana.java
+++ src/it/beije/malang/natura/Rana.java
@@ -1,5 +1,17 @@
 package it.beije.malang.natura;
 
-public class Rana {
+public class Rana extends Anfibi implements Carnivoro {
+
+	@Override
+	public void mangiare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mangiaCarne() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
