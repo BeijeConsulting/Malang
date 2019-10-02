54e764dc1e39eb7a61569302ce5c9bf7a25daa4f
Padawan01 <Padawan01@DESKTOP-UNEQSMN>
2019-10-02 15:30:51 +0200

    classe implementa

---
+++ src/it/beije/malang/natura/piranha.java
@@ -0,0 +1,19 @@
+package it.beije.malang.natura;
+
+public class piranha implements Erbivoro, Onnivoro {
+
+
+	public void mangiaCarne() {
+		
+		System.out.println("la maggior parte dei  piranha  mangia la carne");
+	}
+
+
+	public void mangiaErba() {
+	
+		System.out.println("alucni piranha sono erbivori");
+	}
+	
+	
+
+}
