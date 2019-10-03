dae9c45de2eaf08b2d8e6494468fa0a3d1ad8359
Fulginiti <Fulginiti@LAPTOP-EE4JK8P8>
2019-10-03 17:04:00 +0200

    uomo

--- src/it/beije/malang/natura/Uomo.java
+++ src/it/beije/malang/natura/Uomo.java
@@ -3,10 +3,11 @@ package it.beije.malang.natura;
 public class Uomo extends Mammifero implements Onnivoro{
 	
 	public static void main(String[] args) {
-		
+		System.out.println("sono un uomo e...: \n");
 		Uomo u = new Uomo();
 		u.Parto();
 		u.Onnivoro();
+		
 	}
 	
 }
