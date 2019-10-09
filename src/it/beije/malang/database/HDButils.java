85a1b5e7a25b58cf29999a41feca592368c8f4b8
ivfly <iv0fly@gmail.com>
2019-10-09 15:08:38 +0200

    Criteria

--- src/it/beije/malang/database/HDButils.java
+++ src/it/beije/malang/database/HDButils.java
@@ -1,8 +1,13 @@
 package it.beije.malang.database;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 
 import it.beije.malang.Contatto;
@@ -21,15 +26,27 @@ public class HDButils {
 		
 		Session session = factory.openSession();
 		
-		String hql = "SELECT c FROM Contatto as c";
-		Query<Contatto> query = session.createQuery(hql);
-		for (Contatto contatto : query.list()) {
+//		String hql = "SELECT c FROM Contatto as c";
+//		Query<Contatto> query = session.createQuery(hql);
+//		for (Contatto contatto : query.list()) {
+//			System.out.println("id : " + contatto.getId());
+//			System.out.println("nome : " + contatto.getNome());
+//			System.out.println("cognome : " + contatto.getCognome());
+//			System.out.println("telefono : " + contatto.getTelefono());
+//			System.out.println("email : " + contatto.getEmail());
+//		}
+		
+		Criteria criteria = session.createCriteria(Contatto.class);
+		List<Contatto> contatti = criteria.add(Restrictions.eq("nome", "rossi")).list();
+
+		for (Contatto contatto : contatti) {
 			System.out.println("id : " + contatto.getId());
 			System.out.println("nome : " + contatto.getNome());
 			System.out.println("cognome : " + contatto.getCognome());
 			System.out.println("telefono : " + contatto.getTelefono());
 			System.out.println("email : " + contatto.getEmail());
 		}
+
 		
 		session.close();
 		factory.close();
