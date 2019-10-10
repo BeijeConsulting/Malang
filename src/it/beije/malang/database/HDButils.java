70face848dd582730fb7d22a285345383e59fbb6
simoneagostini <1996enomis@gmail.com>
2019-10-10 17:00:51 +0200

    asd

--- src/it/beije/malang/database/HDButils.java
+++ src/it/beije/malang/database/HDButils.java
@@ -1,16 +1,17 @@
 package it.beije.malang.database;
 
-<<<<<<< HEAD
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-=======
+
 import java.util.Iterator;
 import java.util.List;
->>>>>>> refs/remotes/origin/master
+
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -36,18 +37,18 @@ public class HDButils {
 		
 		Session session = factory.openSession();
 		Transaction transaction = session.beginTransaction();
-		
-<<<<<<< HEAD
-	String hql = "SELECT c FROM Contatto as c";
-		Query<Contatto> query = session.createQuery(hql);
-	for (Contatto contatto : query.list()) {
-		System.out.println("id : " + contatto.getId());
-		System.out.println("nome : " + contatto.getNome());
-		System.out.println("cognome : " + contatto.getCognome());
-		System.out.println("telefono : " + contatto.getTelefono());
-		System.out.println("email : " + contatto.getEmail());
-		}
-=======
+//		
+
+//	String hql = "SELECT c FROM Contatto as c";
+//		Query<Contatto> query = session.createQuery(hql);
+//	for (Contatto contatto : query.list()) {
+//		System.out.println("id : " + contatto.getId());
+//		System.out.println("nome : " + contatto.getNome());
+//		System.out.println("cognome : " + contatto.getCognome());
+//		System.out.println("telefono : " + contatto.getTelefono());
+//		System.out.println("email : " + contatto.getEmail());
+//		}
+
 //		String hql = "SELECT c FROM Contatto as c";
 //		Query<Contatto> query = session.createQuery(hql);
 //		for (Contatto contatto : query.list()) {
@@ -57,14 +58,14 @@ public class HDButils {
 //			System.out.println("telefono : " + contatto.getTelefono());
 //			System.out.println("email : " + contatto.getEmail());
 //		}
->>>>>>> refs/remotes/origin/master
+
 		
-<<<<<<< HEAD
-		@SuppressWarnings("deprecation")
-		Criteria criteria = session.createCriteria(Contatto.class);
-		@SuppressWarnings("unchecked")
-		List<Contatto> Contatti = criteria.add(Restrictions.eq("cognome", "Agostini")).list();
-		@SuppressWarnings("rawtypes")
+
+//		@SuppressWarnings("deprecation")
+//		Criteria criteria = session.createCriteria(Contatto.class);
+//		@SuppressWarnings("unchecked")
+//		List<Contatto> Contatti = criteria.add(Restrictions.eq("cognome", "Agostini")).list();
+//		@SuppressWarnings("rawtypes")
 		
 		
 
@@ -72,16 +73,16 @@ public class HDButils {
 		
 
 		
-		
-		Iterator itr = Contatti.iterator();		
-		while (itr.hasNext()) {
-//			Transaction transaction = session.beginTransaction();
-			Contatto contatto = (Contatto) itr.next();
-			System.out.println(contatto.getId());
-			System.out.println(contatto.getCognome());
-		
-		}
-=======
+//		
+//		Iterator itr = Contatti.iterator();		
+//		while (itr.hasNext()) {
+////			Transaction transaction = session.beginTransaction();
+//			Contatto contatto = (Contatto) itr.next();
+//			System.out.println(contatto.getId());
+//			System.out.println(contatto.getCognome());
+//		
+//		}
+
 //		Criteria criteria = session.createCriteria(Contatto.class);
 //		List<Contatto> contatti = criteria.list();//.add(Restrictions.eq("nome", "pippo"))
 //
@@ -96,22 +97,87 @@ public class HDButils {
 //				contatto.setCognome("Rossi");
 //			}
 //		}
-		
-		Contatto contatto = new Contatto();
-		contatto.setNome("Fiorenza");
-		contatto.setCognome("Riccio");
-		contatto.setEmail("fiore@riccio.it");
-		contatto.setTelefono("34556616");
 
-		System.out.println("id : " + contatto.getId());
-		session.save(contatto);
-		System.out.println("id : " + contatto.getId());
 		
-		transaction.commit();
->>>>>>> refs/remotes/origin/master
+	    Configuration cfg = new Configuration();
+	    cfg.configure("hibernate.cfg.xml");
+
+//	    SessionFactory factory = cfg.buildSessionFactory();
+//	    Session session = factory.openSession();
+
+	   File file = new File("C:/Users/Padawan06/Desktop/csvconhibernate.txt"); 
+
+	    FileWriter fw = new FileWriter(file);
+	    BufferedWriter bw = new BufferedWriter(fw);
+
+
+	    Query qry = session.createQuery("SELECT c FROM Contatto as c");
+	    List l =(List) qry.list();
+	    System.out.println("Total Number Of Records : "+((java.util.List) l).size());
+	    Iterator it = ((java.util.List) l).iterator();
+
+//	    while(it.hasNext())
+//	    {
+//	        Object o = (Object)it.next();
+//	        Employee_details e = (Employee_details)o;
+//	        System.out.println("Employee ID : "+e.getId());
+//	        System.out.println("Employee Name : "+e.getName());
+//	        System.out.println("Employee Salary : "+ e.getSalary());
+//	        System.out.println("----------------------");
+//	        bw.write(e.getId());
+	        
+	        
+			Criteria criteria = session.createCriteria(Contatto.class);
+			List<Contatto> contatti = criteria.list();//.add(Restrictions.eq("nome", "pippo"))
+	        
+			for (Contatto contatto : contatti) {
+			System.out.println("id : " + contatto.getId());
+			System.out.println("nome : " + contatto.getNome());
+			System.out.println("cognome : " + contatto.getCognome());
+			System.out.println("telefono : " + contatto.getTelefono());
+			System.out.println("email : " + contatto.getEmail());
+			
+			
+			if (contatto.getNome().equals("Giuseppe")) {
+				contatto.setCognome("Rossi");
+			}
+		}
+	        
+	        // Here instead of Id some junk value is getting inserted
+	      
+	    bw.flush();
+	    bw.close();
+
+	    
+	    
 		session.close();
 		factory.close();
 		System.out.println("is open?" + factory.isOpen());
+	    
+	    }
 	
+		
+		
+		
+		
+		
+//		Contatto contatto = new Contatto();
+//		contatto.setNome("Fiorenza");
+//		contatto.setCognome("Riccio");
+//		contatto.setEmail("fiore@riccio.it");
+//		contatto.setTelefono("34556616");
+//
+//		
+//		System.out.println("id : " + contatto.getId());
+//		session.save(contatto);
+//		System.out.println("id : " + contatto.getId());
+		
+		
+		
+//		transaction.commit();
+
+	
+		
+		
 	}
-}
+
