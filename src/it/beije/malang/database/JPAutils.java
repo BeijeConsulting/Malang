f71e7270aa1c7318016b097742ab9c4ad99b8679
ivfly <iv0fly@gmail.com>
2019-10-11 17:17:02 +0200

    relazione tra entities

--- src/it/beije/malang/database/JPAutils.java
+++ src/it/beije/malang/database/JPAutils.java
@@ -1,12 +1,14 @@
 package it.beije.malang.database;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import it.beije.malang.database.entities.Contatto;
+import it.beije.malang.database.entities.Indirizzo;
 import it.beije.malang.database.entities.Utente;
 
 
@@ -18,21 +20,34 @@ public class JPAutils {
 		EntityManager entityManager = factory.createEntityManager();
 		
 		int id = 1;
-		Contatto c = entityManager.find(Contatto.class, id);
+		Utente u = entityManager.find(Utente.class, id);
 
-		System.out.println("Contatto : " + c.getNome());
+		System.out.println("Utente : " + u.getNome());
+		System.out.println("Utente indirizzi : " + u.getIndirizzi());
+		for (Indirizzo i : u.getIndirizzi()) {
+			System.out.println("id : " + i.getId());
+			System.out.println("cap : " + i.getCap());
+			System.out.println("citta : " + i.getCitta());
+			System.out.println("prov : " + i.getProvincia());
+		}
 
-		Utente u = new Utente();
-		u.setCognome("Ferulli");
-		u.setNome("Marina2");
-		u.setEmail("marina2@ferulli.it");
-		
-		entityManager.getTransaction().begin();
-
-		System.out.println("Utente id : " + u.getId());
-		entityManager.persist(u);
-		System.out.println("Utente id : " + u.getId());
-		entityManager.getTransaction().commit();
+////		Utente u = new Utente();
+////		u.setCognome("Ferulli");
+////		u.setNome("Marina3");
+////		u.setEmail("marina3@ferulli.it");
+////		
+//		Indirizzo indirizzo = new Indirizzo();
+//		indirizzo.setCap("20100");
+//		indirizzo.setCitta("Milano");
+//		indirizzo.setProvincia("MI");
+//		indirizzo.setTelefono("23224");
+//						
+//		entityManager.getTransaction().begin();
+//
+//		System.out.println("Utente id : " + u.getId());
+//		entityManager.persist(u);
+//		System.out.println("Utente id : " + u.getId());
+//		entityManager.getTransaction().commit();
 	
 		entityManager.close();
 
