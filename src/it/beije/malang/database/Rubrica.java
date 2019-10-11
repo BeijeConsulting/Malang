d3370074eaf3394fba4ecf5c4073c662d7e2b724
Padawan01 <Padawan01@DESKTOP-UNEQSMN>
2019-10-11 15:38:10 +0200

    ...

--- src/it/beije/malang/database/Rubrica.java
+++ src/it/beije/malang/database/Rubrica.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.beije.malang.Contatto;
+import it.beije.malang.database.entities.Contatto;
 
 public class Rubrica {
 
---
+++ src/it/beije/malang/database/entities/Utente.java
@@ -0,0 +1,67 @@
+package it.beije.malang.database.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "utenti")
+public class Contatto {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "cognome")
+	private String cognome;
+	
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "email")
+	private String email;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}	
+
+
+	// create table utenti (
+	//		id integer not null auto_increment,
+	//	    cognome varchar(255),
+	//	    email varchar(255),
+	//	    nome varchar(255),
+	//	    primary key (id)
+	//	)
